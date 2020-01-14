54c92b85f3d502c7f69aa9b4922aff9a52da842a
Jeffrey Chow <chow1340@mylaurier.ca>
2020-01-14 14:24:31 -0500

    deploy

---
+++ build/precache-manifest.6948340f3f8f40ce96df04596b3a4566.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3059b2802db878b62690118f40f116",
+    "url": "/paper-dashboard-react/index.html"
+  },
+  {
+    "revision": "baa6f959fc59e2ff53b3",
+    "url": "/paper-dashboard-react/static/css/2.151d002d.chunk.css"
+  },
+  {
+    "revision": "7be4750418ea3af05e23",
+    "url": "/paper-dashboard-react/static/css/main.6e0c5205.chunk.css"
+  },
+  {
+    "revision": "baa6f959fc59e2ff53b3",
+    "url": "/paper-dashboard-react/static/js/2.ae93cc29.chunk.js"
+  },
+  {
+    "revision": "7be4750418ea3af05e23",
+    "url": "/paper-dashboard-react/static/js/main.426efd56.chunk.js"
+  },
+  {
+    "revision": "b3fe53a14804cfc5877b",
+    "url": "/paper-dashboard-react/static/js/runtime~main.ff95ff9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paper-dashboard-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
